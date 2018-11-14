5ff8cbc0c4433ef96e54e84d00067be865c62668
unknown <yingwenwang@snsshop.net>
2018-11-14 17:29:08 +0800

    第三分包

--- vendor/github.com/streadway/amqp/fuzz.go
+++ vendor/github.com/streadway/amqp/fuzz.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // +build gofuzz
 
 package amqp
@@ -16,22 +15,3 @@ func Fuzz(data []byte) int {
 	}
 	return 1
 }
-=======
-// +build gofuzz
-
-package amqp
-
-import "bytes"
-
-func Fuzz(data []byte) int {
-	r := reader{bytes.NewReader(data)}
-	frame, err := r.ReadFrame()
-	if err != nil {
-		if frame != nil {
-			panic("frame is not nil")
-		}
-		return 0
-	}
-	return 1
-}
->>>>>>> 7563d76138a21269a66175d7cdad4b86cc127ec9
