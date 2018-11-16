b3c355d6787fadeb594b2f4800b51a908189699a
unknown <yingwenwang@snsshop.net>
2018-11-16 10:24:14 +0800

    代码优化

--- module/memberInfo.go
+++ module/memberInfo.go
@@ -24,38 +24,6 @@ func (m *MemberInfo) TableName() string {
 	return "member_info"
 }
 
-//func (m *MemberInfo) SendMessageForSex(manageId int,sex, message string) error {
-//	common.Log.Infoln("根据性别发送短息")
-//	engine := common.DB
-//	list := make([]MemberInfo, 0)
-//	fmt.Println("sex", sex, "message", message)
-//	err := engine.Where("sex=?", sex).Find(&list)
-//	if err != nil {
-//		common.Log.Errorln(err)
-//		return err
-//	}
-//	//发送短信
-//	for _, v := range list {
-//		sendLog:=new(TemplateSmsLog)
-//		sendLog.TemplateManageId =manageId
-//		sendLog.MemberId = v.MemberId
-//		sendLog.CorpId = v.CorpId
-//		sendLog.Mobile = v.Mobile
-//		sendLog.MallId = new(Member).GetMallId(v.MemberId)
-//		if sendLog.MallId ==0{
-//			continue
-//		}
-//
-//		result,err:=sendLog.marshalJson(message)
-//		if err!=nil{
-//			common.Log.Errorln(err)
-//			continue
-//		}
-//		Push("myPusher", "rmq_test", result)
-//	}
-//
-//	return nil
-//}
 
 func(m*MemberInfo)GetMessageOfSex(sex string)([]MemberInfo,error){
 	common.Log.Infof("性别为%q的会员\r\n",sex)
@@ -68,35 +36,7 @@ func(m*MemberInfo)GetMessageOfSex(sex string)([]MemberInfo,error){
 	return list,nil
 }
 
-//func (m *MemberInfo) SendMessageEveryOne(manageId int,message string) error {
-//	common.Log.Infoln("即时全员发送")
-//	engine := common.DB
-//	list := make([]MemberInfo, 0)
-//	err := engine.Select(" * ").Find(&list)
-//	if err != nil {
-//		common.Log.Warnln(err)
-//		log.Print(err.Error())
-//		return err
-//	}
-//	for _, v := range list {
-//		sendLog:=new(TemplateSmsLog)
-//		sendLog.TemplateManageId =manageId
-//		sendLog.MemberId = v.MemberId
-//		sendLog.CorpId = v.CorpId
-//		sendLog.Mobile = v.Mobile
-//		sendLog.MallId = new(Member).GetMallId(v.MemberId)
-//		if sendLog.MallId ==0{
-//			continue
-//		}
-//		result,err:=sendLog.marshalJson(message)
-//		if err!=nil{
-//			common.Log.Errorln(err)
-//			continue
-//		}
-//		Push("myPusher", "rmq_test", result)
-//	}
-//	return nil
-//}
+
 
 func (m* MemberInfo)GetAllMember()([]MemberInfo,error){
 	common.Log.Infoln("即时全员发送")
@@ -109,78 +49,7 @@ func (m* MemberInfo)GetAllMember()([]MemberInfo,error){
 	return list,nil
 }
 
-//按照会员生日发送
-//func (m *MemberInfo) SendMessageOfBirthDay(manageId int,birthDat, message string) error {
-//	common.Log.Infoln("按照生日月分发送")
-//	subList := strings.Split(birthDat, ",")
-//	engine := common.DB
-//	list := make([]MemberInfo, 0)
-//	err := engine.Find(&list)
-//	if err != nil {
-//		common.Log.Errorln(err)
-//		return err
-//	}
-//	for _, v := range list {
-//		if len(v.Birthday) != len("1991-06-14") {
-//			continue
-//		}
-//		month := v.Birthday[5:7]
-//		for _, m := range subList {
-//			if strings.Compare(month, m) == 0 || strings.Compare(month, "0"+m) == 0 {
-//				sendLog:=new(TemplateSmsLog)
-//				sendLog.TemplateManageId =manageId
-//				sendLog.MemberId = v.MemberId
-//				sendLog.CorpId = v.CorpId
-//				sendLog.Mobile = v.Mobile
-//				sendLog.MallId = new(Member).GetMallId(v.MemberId)
-//				if sendLog.MallId ==0{
-//					continue
-//				}
-//				result,err:=sendLog.marshalJson(message)
-//				if err !=nil{
-//					common.Log.Errorln(err)
-//					continue
-//				}
-//				Push("myPusher","rmq_test",result)
-//				continue
-//			}
-//		}
-//	}
-//	return nil
-//}
 
-
-//指定电话号码发送短息
-//func (m *MemberInfo) SendMessageOfPhone(manageId int,Phone, message string) error {
-//	common.Log.Infoln("指定电话号码发送短息")
-//	engine := common.DB
-//	has, err := engine.Where("mobile=?", Phone).Get(m)
-//	if err != nil {
-//		common.Log.Errorln(err)
-//		return err
-//	}
-//	if !has {
-//		err = fmt.Errorf("手机号码为：%v的用户不存在！！", Phone)
-//		common.Log.Errorln(err)
-//		return err
-//	}
-//	sendLog:=new(TemplateSmsLog)
-//	sendLog.TemplateManageId =manageId
-//	sendLog.MemberId = m.MemberId
-//	sendLog.CorpId = m.CorpId
-//	sendLog.Mobile = m.Mobile
-//	sendLog.MallId = new(Member).GetMallId(m.MemberId)
-//	if sendLog.MallId ==0{
-//		return fmt.Errorf("会员%d不存在！！",m.MemberId)
-//	}
-//	result,err:=sendLog.marshalJson(message)
-//	if err!=nil{
-//		common.Log.Errorln(err)
-//		return err
-//	}
-//	Push("myPusher","rmq_test",result)
-//	return nil
-//}
 
 func (m*MemberInfo)GetMessageOfPhone(phone string) error {
 	common.Log.Infoln("指定电话号码发送短息")
