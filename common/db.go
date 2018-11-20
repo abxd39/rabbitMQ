02f15cb10d7c1ef71396339077c4cda16c91d96d
unknown <yingwenwang@snsshop.net>
2018-11-20 11:44:35 +0800

    debug

--- common/db.go
+++ common/db.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
-	"sctek.com/typhoon/th-open-api/common"
 )
 
 func OpenDb() error {
 	var err error
 	str:= fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", Config.Db.User, Config.Db.PassWord, Config.Db.Host, Config.Db.Port, Config.Db.Name)
-	common.Log.Errorln("数据库链接",str)
+	fmt.Println("数据库链接",str)
 	DB, err = xorm.NewEngine("mysql",str)
 	if err!=nil{
 		panic(err)
