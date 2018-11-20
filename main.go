88d69d797fa088f5e35924dde037f0ddc8e75cad
unknown <yingwenwang@snsshop.net>
2018-11-20 18:17:55 +0800

    “修改定时任务

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/robfig/cron"
 	"log"
 	"net/http"
 	"os"
@@ -35,19 +36,24 @@ func main() {
 			log.Printf("listen: %s\n", err)
 		}
 	}()
+	//启动定时器
+	cron := cron.New()
+	cron.Start()
+	defer cron.Stop()
+	//5秒执行一次
+	cron.AddFunc("*/5 * * * * *", service.CronSelect)
 	//mq 初始化
 	common.CheckErr(service.Init())
 	//工作池初始化
 	service.InitPool()
 	defer service.ClosePool()
-	defer  service.Fini()
+	defer service.Fini()
 	common.CheckErr(service.Receive())
 	//Wait for interrupt signal to gracefully shutdown the server with
 	//a timeout of 30 seconds.
 	quit := make(chan os.Signal)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
-
 
 	log.Println("Shutdown Server ...")
 	//stop http listen
